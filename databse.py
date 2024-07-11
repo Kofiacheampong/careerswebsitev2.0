3f8210f5825381d71349227aeb1ef48addb36e64
Kofiacheampong <acheampong0805@outlook.com>
2024-07-11 08:22:24 -0500

    edited database functions

--- databse.py
+++ databse.py
@@ -1,68 +1,47 @@
-from sqlalchemy  import create_engine, text
+from sqlalchemy import create_engine, text
 import urllib.parse
 from dotenv import load_dotenv
-import pyodbc
-
 import os
 
+# Load environment variables from .env file
+load_dotenv()
 
-db_connection_string = os.getenv('AZUREDB')
-
-# Replace these values with your Azure SQL database credentials
+# Retrieve environment variables
 server_name = os.getenv('SERVER_NAME')
 database_name = os.getenv('DATABASE_NAME')
 user_name = os.getenv('USER_NAME')
 password = os.getenv('PASSWORD')
 
+# Check if environment variables are loaded correctly
+if not all([server_name, database_name, user_name, password]):
+    raise ValueError("One or more environment variables are missing.")
+
 # Specify the ODBC driver and other options
 params = urllib.parse.quote_plus(
-r"Driver={ODBC Driver 18 for SQL Server};"
-f"Server=tcp:{server_name},1433;"
-f"Database={database_name};"
-f"Uid={user_name};"
-f"Pwd={password};"
-"Encrypt=yes;"
-"TrustServerCertificate=no;"
-"Connection Timeout=30;"
+    f"Driver={{ODBC Driver 18 for SQL Server}};"
+    f"Server=tcp:{server_name},1433;"
+    f"Database={database_name};"
+    f"Uid={user_name};"
+    f"Pwd={password};"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
 )
 
 # Create the engine with the connection string
-engine = create_engine(db_connection_string )
-
-
+db_connection_string = f"mssql+pyodbc:///?odbc_connect={params}"
+engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text('SELECT * FROM JOBS'))
-    
-    jobs = []
-    for row in result.all():
-        row_dict = dict(row._mapping)  # Use _mapping to convert the row to a dictionary
-        jobs.append(row_dict)
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM JOBS'))
+        jobs = [dict(row._mapping) for row in result.all()]  # Use _mapping to convert rows to dictionaries
     return jobs
-  
-  
-  
-  
-# try:
-#     connection = engine.connect()
-#     print("Connection successful!")
-#     connection.close()
-# except Exception as e:
-#     print(f"Error connecting: {e}")
-    
-
-  
-    # print ('type(result):', type(result)) 
-    # result_all = result.all()
-    # print ('type(result.all()):', type(result_all)) 
-    # first_result = result_all[0]
-    # print ('type(first_result):', type(first_result)) 
-    # first_result_dict = first_result._asdict()
-    # print('type(first_result_dict):', type(first_result_dict))
-    # print(first_result_dict)
-
-  
-
 
 # Test the connection
+try:
+    connection = engine.connect()
+    print("Connection successful!")
+    connection.close()
+except Exception as e:
+    print(f"Error connecting: {e}")
